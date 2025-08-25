96876039f0d659393187d24ab2c29d32479aff91
jualmd <rm95207@estudante.fieb.edu.br>
2025-08-25 10:43:39 -0300

    a

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
+
+const App = () => {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="pokeflex" element={<PokeFlex/>} />
+      <Route path="pokegrid" element={<PokeGrid/>} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,17 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
+import PokeFlex from './PokeFlex'
+import PokeGrid from './PokeGrid'
+
+const Home = () => {
+    
+    return (
+        <div>
+            <PokeFlex />
+            <PokeGrid />
+        </div>
+    )
+}
+
+export default Home
