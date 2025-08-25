d26ed4447ddc063cef26a9c10af0f29d1ed32c9b
jualmd <rm95207@estudante.fieb.edu.br>
2025-08-25 11:06:15 -0300

    Menu

---
+++ src/Menu.jsx
@@ -0,0 +1,28 @@
+const Menu = () => {
+    return (
+        <div>
+        <ul className="nav">
+            <li className="nav-item">
+                <a class="nav-link active" href="">
+                    Active
+                </a>
+            </li>
+            <li>
+                <li clasName="nav-item">
+                    <a className= "nav-link" href="/home">
+                        Home
+                    </a>
+                </li>
+            </li>
+            <li clasName="nav-item">
+                <a className= "nav-link" href="/viacep">
+                 ViaCep
+
+                </a>
+            </li>
+
+        </ul>
+        
+        </div>
+    )
+}
